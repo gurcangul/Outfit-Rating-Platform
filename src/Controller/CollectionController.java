6076e523b601d3f20a6d11e2c3bfc76d71e7c2a2
gurcangul <grcngl35@gmail.com>
2021-05-02 17:27:23 +0300

    Collection contoller added

--- src/Controller/CollectionController.java
+++ src/Controller/CollectionController.java
@@ -1,5 +1,71 @@
 package Controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import Entities.Collection;
+import Entities.User;
+import GUI.MainFrame;
+import GUI.MenuPanel;
+import GUI.ViewCollectionPanel;
+import Observer.Data;
+
 public class CollectionController {
+    private final User user;
+    private final ViewCollectionPanel viewCollectionPanel;
+    private final List<Collection> allCollections;
+    private final Data data;
+    private MainFrame mainFrame;
+
+    public CollectionController(User user, ViewCollectionPanel viewCollectionPanel) {
+        this.user = user;
+        this.viewCollectionPanel = viewCollectionPanel;
+        this.data = new Data();
+
+        user.attach(viewCollectionPanel);
+        user.attach(viewCollectionPanel);
+
+        viewCollectionPanel.viewAllActionListener(new createCollectionActionListener());
+        /*viewCollectionPanel.addOpenCollectionButton(new openCollectionActionListener());
+        viewCollectionPanel.addMainMenuActionListener(new mainMenuActionListener());
+        viewCollectionPanel.addOpenCollectionMouseListener(new openCollectionMouseListener());*/
+
+        this.allCollections = new ArrayList<>();
+        allCollections.addAll(user.getCollectionList());
+        List<Collection> followingsCollections = new ArrayList<>();
+        for(String username: user.getFollowingList()){
+            System.out.println(username);
+            followingsCollections.addAll(data.getUser(username).getCollectionList());
+        }
+        allCollections.addAll(followingsCollections);
+        viewCollectionPanel.addCollectionLists(followingsCollections);
+    }
+
+    private class createCollectionActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+    }
+
+    private class openCollectionActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        }
+    }
+
+    private class mainMenuActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            MenuPanel menuPanel = new MenuPanel(viewCollectionPanel.getMainFrame());
+            MenuController menuController = new MenuController(menuPanel, user);
+        }
+    }
+
+ 
 
 }
--- src/GUI/ViewCollectionPanel.java
+++ src/GUI/ViewCollectionPanel.java
@@ -2,8 +2,10 @@ package GUI;
 
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
-import Observer.Observer;
+import java.util.ArrayList;
+import java.util.List;
 
+import Observer.Observer;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -22,20 +24,64 @@ public class ViewCollectionPanel extends JPanel implements Observer {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Collection collection;
-    private MainFrame mainFrame;
     private User user;
+    private MainFrame mainFrame;
+    private List<Collection> collectionLists;
 
-
+    JList<String> collectionsList;
+    JButton allCollection;
 
 	public ViewCollectionPanel(MainFrame mainFrame, Collection collection) {
-        setMainFrame(mainFrame); 
-        setCollection(collection);
-		JPanel jP=  new JPanel(new GridLayout(3, 1));
+        this.setMainFrame(mainFrame); 
+        JPanel jP=  new JPanel(new GridLayout(3, 1));
 		jP.setLayout(null);
+        setLayout(null);
+		
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(10, 37, 590, 161);
+
+        add(scrollPane);
+        
+        collectionsList = new JList();
+        scrollPane.setViewportView(collectionsList);
+        
+        allCollection = new JButton("All Collections");
+        allCollection.setBounds(37, 232, 114, 21);
+        add(allCollection);
+        
+        JButton btnNewButton_1 = new JButton("My Collections");
+        btnNewButton_1.setBounds(175, 232, 114, 21);
+        add(btnNewButton_1);
+        
+        JButton btnNewButton_2 = new JButton("Following");
+        btnNewButton_2.setBounds(305, 232, 114, 21);
+        add(btnNewButton_2);
+        
+        JButton btnNewButton_2_1 = new JButton("Not Following");
+        btnNewButton_2_1.setBounds(429, 232, 114, 21);
+        add(btnNewButton_2_1);
+        showComponents();
+        
+        setCollection(collection);
+		
+	
+		
         mainFrame.addNewPanel(jP);
+        
+        
     }
   
-
+    private void showComponents() {
+        List<Collection> allCollections = new ArrayList();
+        allCollections.addAll(user.getCollectionList());
+        String[] CollectionsArr = new String[allCollections.size()];
+        int i = 0;
+        for(Collection wl: allCollections){
+            CollectionsArr[i] = "<html><body>" + wl.getName() + "<br>" + " " + "<br>" + "</span></body></html>}";
+            i++;
+        }
+        collectionsList.setListData(CollectionsArr);
+    }
 	public Collection getCollection() {
 		return collection;
 	}
@@ -44,13 +90,6 @@ public class ViewCollectionPanel extends JPanel implements Observer {
 		this.collection = collection;
 	}
 
-	public MainFrame getMainFrame() {
-		return mainFrame;
-	}
-
-	public void setMainFrame(MainFrame mainFrame) {
-		this.mainFrame = mainFrame;
-	}
 
 	@Override
 	public void update(Object arg) {
@@ -67,4 +106,30 @@ public class ViewCollectionPanel extends JPanel implements Observer {
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
+	public void viewAllActionListener(ActionListener actionListener) {
+		//allCollection.addActionListener(actionListener);
+		//showComponents();
+		
+    }
+	
+	public void addCollectionLists(List<Collection> collectionList){
+		  collectionLists.addAll(collectionList);
+		  showComponents();
+	}
+	
+	
+	
+	 public void addCreateCollectionButton(ActionListener actionListener) {
+		 allCollection.addActionListener(actionListener);
+	    }
+
+	public MainFrame getMainFrame() {
+		return mainFrame;
+	}
+
+	public void setMainFrame(MainFrame mainFrame) {
+		this.mainFrame = mainFrame;
+	}
+	
 }
